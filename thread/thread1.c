280ae10d9afdc2a9d86c7e1e19f899023094cc21
fLexsooP <ascc594557067@gmail.com>
2023-02-15 14:35:55 -0800

    add thread example

---
+++ thread/thread1.c
@@ -0,0 +1,29 @@
+
+#include <pthread.h>
+#include <stdio.h>
+
+void* thread_func(void* arg) {
+
+	printf("New thread\n");
+	return NULL;
+}
+
+int main() {
+
+	pthread_t tid_original_thread;
+	tid_original_thread = pthread_self();
+
+	printf("Thread id of original thread %lu\n", tid_original_thread);
+
+	pthread_t tid_new_thread;
+
+	pthread_create(&tid_new_thread, NULL, thread_func, NULL);
+
+	printf("Thread id of created thread %lu\n", tid_new_thread);
+
+	pthread_join(tid_new_thread, NULL);
+
+	printf("Original thread\n");
+
+	return 0;
+}
