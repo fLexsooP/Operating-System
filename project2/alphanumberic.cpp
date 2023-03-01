843ce80bdd2642c3da11fd5ef03c61b28b2c8a91
fLexsooP <ascc594557067@gmail.com>
2023-02-28 21:10:07 -0800

    save

--- project2/alphanumberic.cpp
+++ project2/alphanumberic.cpp
@@ -5,42 +5,50 @@
 #include <pthread.h>
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include <unistd.h>
+#include <ctype.h>
 #include <bits/stdc++.h>
+
 
 using namespace std;
 
-string phrase = "";
-istringstream ss;
-bool locked;
+string phrase;
+vector<string> words;
+int cur_index = 0;
+bool locked = true;
+
+vector<string> split_into_words(string phrase) {
+    stringstream ss(phrase);
+    string word;
+    vector<string> words;
+    while (ss >> word) {
+        words.push_back(word);
+    }
+    return words;
+}
 
 void* print_alpha(void* arg) {
     char* message;
     message = (char*)arg;
 
-    string word;
-
-    while(ss >> word) {
-        if (word[0] < '0' || word[0] > '9') {
-            printf("%s: ", message);
-            cout << word << endl;
-        }
+    while (cur_index < words.size()) {
+        if (isalpha(words[cur_index][0])) {
+            cout << message << ": " << words[cur_index] << endl;
+            cur_index++;
+        } 
     }
     pthread_exit(0);
 }
-
-
 
 void* print_numeric(void* arg) {
     char* message;
     message = (char*)arg;
 
-    string word;
-
-    while(ss >> word) {
-        if (word[0] >= '0' && word[0] <= '9') {
-            printf("%s: ", message);
-            cout << word << endl;
+    while (cur_index < words.size()) {
+        if (isdigit(words[cur_index][0])) {
+            cout << message << ": " << words[cur_index] << endl;
+            cur_index++;
         }
     }
     pthread_exit(0);
@@ -57,11 +65,12 @@ int main(int argc, char const *argv[])
 
 	/* Save the name of the file */
 	phrase = argv[1];
-    ss.str(phrase);
+    words = split_into_words(phrase);
+    
+
+
 
     pthread_t alpha, numeric;
-    // char* alpha_message = const_cast<char*>(a_message.c_str());
-    // char* numeric_message = const_cast<char*>(n_message.c_str());
     char* alpha_message = const_cast<char*>("alpha");
     char* numeric_message = const_cast<char*>("numeric");
     int  alpha_ret, num_ret;
@@ -70,7 +79,6 @@ int main(int argc, char const *argv[])
     num_ret = pthread_create(&numeric, NULL, print_numeric, (void*)numeric_message);
 
 
-    
     pthread_join(alpha, NULL);
     pthread_join(numeric, NULL);
 
